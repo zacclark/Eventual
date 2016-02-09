972a9b1585de4450fbb70f2bb79638203a6ecb5d
pivotal <pivotal@winston.sf.pivotallabs.com>
2016-02-09 10:23:28 -0800

    Initial Commit

---
+++ Eventual/Eventual.h
@@ -0,0 +1,19 @@
+//
+//  Eventual.h
+//  Eventual
+//
+//  Created by pivotal on 2/9/16.
+//  Copyright © 2016 Zac Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Eventual.
+FOUNDATION_EXPORT double EventualVersionNumber;
+
+//! Project version string for Eventual.
+FOUNDATION_EXPORT const unsigned char EventualVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Eventual/PublicHeader.h>
+
+
