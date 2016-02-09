45a0b741faec0adcc75a66438810283c7092d34b
Zac Clark <hi@zacclark.com>
2016-02-09 15:36:43 -0800

    I'm really a popularizer at heart

--- Eventual/Eventual.swift
+++ Eventual/Eventual.swift
@@ -3,12 +3,12 @@ public class Eventual<T> {
     private var value: T?
     private var effects: [Effect]
     
-    init() {
+    public init() {
         value = nil
         effects = []
     }
     
-    init(_ v: T) {
+    public init(_ v: T) {
         value = v
         effects = []
     }
@@ -24,7 +24,7 @@ public class Eventual<T> {
         }
     }
     
-    func finally(f: T -> Void) {
+    public func finally(f: T -> Void) {
         if let v = self.value {
             f(v)
             return
@@ -32,7 +32,7 @@ public class Eventual<T> {
         effects.append(f)
     }
     
-    func map<U>(f: T -> U) -> Eventual<U> {
+    public func map<U>(f: T -> U) -> Eventual<U> {
         let otherE = Eventual<U>()
         finally { t in
             otherE.resolve(f(t))
@@ -40,7 +40,7 @@ public class Eventual<T> {
         return otherE
     }
     
-    func bind<U>(f: T -> Eventual<U>) -> Eventual<U> {
+    public func bind<U>(f: T -> Eventual<U>) -> Eventual<U> {
         let otherE = Eventual<U>()
         finally { t in
             let newE = f(t)
@@ -53,11 +53,11 @@ public class Eventual<T> {
 // MARK: - Convenience extenion for simpler naming
 
 extension Eventual {
-    func then<U>(f: T -> U) -> Eventual<U> {
+    public func then<U>(f: T -> U) -> Eventual<U> {
         return map(f)
     }
     
-    func then<U>(f: T -> Eventual<U>) -> Eventual<U> {
+    public func then<U>(f: T -> Eventual<U>) -> Eventual<U> {
         return bind(f)
     }
 }
@@ -67,11 +67,11 @@ extension Eventual {
 public struct Resolver<T> {
     let eventual: Eventual<T>
     
-    init() {
+    public init() {
         eventual = Eventual<T>()
     }
     
-    func resolve(v: T) {
+    public func resolve(v: T) {
         eventual.resolve(v)
     }
 }
